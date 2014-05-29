0f1032269c8513091fd2753ee9afce86bbfa3ad3
RedFrameSoftware <DavidBDickey@gmail.com>
2014-05-29 17:46:53 -0400

    Create Color.cs

---
+++ Theia/ImageProcessing.Core/Color.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImageProcessing.Core
+{
+    public class Color
+    {
+        public static byte[] ConvertToBlackAndWhite(byte[] imageBytes)
+        {
+            return null;
+        }
+    }
+}
