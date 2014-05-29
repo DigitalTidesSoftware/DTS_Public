0bca345f4430932a1274a4a2cc72efe94bcef562
RedFrameSoftware <DavidBDickey@gmail.com>
2014-05-29 17:47:33 -0400

    Create HistogramEquilization.cs

---
+++ Theia/ImageProcessing.Core/HistogramEquilization.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImageProcessing.Core
+{
+    public static class HistogramEquilization
+    {
+        #region Constructors
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Procedures
+        public static byte[] Equalize(byte[] originalArray)
+        {
+            return null;
+        }
+        #endregion
+    }
+}
